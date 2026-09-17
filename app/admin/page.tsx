6e14db02a870fcfb8d0759191bd0ecdc3275ae10
lauricortazar-droid <lauricortazar@icloud.com>
2026-09-16 21:53:44 -0600

    Corregir conflicto de ruta /admin

--- app/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-import { ProtectedAccess } from "../protected-access";
-import { AdministrationDashboard } from "../administracion/administration-dashboard";
-
-export const dynamic = "force-dynamic";
-
-export default function AdminPage() {
-  return <ProtectedAccess returnTo="/admin" allowedRoles={["admin"]}><AdministrationDashboard /></ProtectedAccess>;
-}
